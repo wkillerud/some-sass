7ff8eaee5eadc20d0f1fe222e89fe7db4bf4b658
William Killerud <william@killerud.com>
2022-05-10 18:37:01 +0200

    Redesign Sassdoc hover
    Put it in a section below divided by a HR, mimicing JSDoc presentation

--- src/unsafe/providers/sassdoc.ts
+++ src/unsafe/providers/sassdoc.ts
@@ -6,76 +6,60 @@ interface ISymbol {
 }
 
 interface ISassDocOptions {
-  displayOptions?: {
+	displayOptions?: {
 		asDocString?: boolean;
-    description?: boolean;
-    author?: boolean;
-    access?: boolean;
-    parameters?: boolean;
-    return?: boolean;
-  };
+		description?: boolean;
+		author?: boolean;
+		access?: boolean;
+		parameters?: boolean;
+		return?: boolean;
+	};
 }
 
 const defaultOptions = {
-  displayOptions: {
-		asDocString: true,
-    description: true,
-    author: true,
-    access: true,
-    parameters: true,
-    return: true,
-  },
+	displayOptions: {
+		description: true,
+		author: true,
+		access: false,
+		parameters: true,
+		return: true,
+		see: true,
+	},
 };
 
 export async function applySassDoc(symbol: ISymbol, identifierType: "function" | "mixin" | "variable", options?: ISassDocOptions): Promise<string> {
 	try {
 		const sassdocs = await sassdoc.parse(symbol.document);
 		if (sassdocs.length) {
-      const name = symbol.info.name.replace("$", "");
-      const displayOptions = options?.displayOptions || defaultOptions.displayOptions;
+			const name = symbol.info.name.replace("$", "");
+			const displayOptions = options?.displayOptions || defaultOptions.displayOptions;
 
 			for (let doc of sassdocs) {
 				if (doc.description && doc.context.type === identifierType && doc.context.name === name) {
 					let description = '';
 
-          if (displayOptions.description) {
-						if (displayOptions.asDocString) {
-							description += doc.description.split("\n").map(line => line ? `/// ${line}` : line).join("\n").trimStart();
-						} else {
-							description += doc.description.trimStart();
-						}
-          }
+					if (displayOptions.description) {
+						description += doc.description.trimStart();
+					}
 
 					if (displayOptions.author && doc.author) {
 						for (let author of doc.author) {
-							if (displayOptions.asDocString) {
-								description += '/// ';
-							}
-							description += `@author ${author}\n`;
+							description += `\n\n@author ${author}`;
 						}
 					}
 
-          if (displayOptions.access) {
-						if (displayOptions.asDocString) {
-							description += '/// ';
-						}
-            description += `@access ${doc.access}\n`;
-          }
+					if (displayOptions.access) {
+						description += `\n\n@access \`${doc.access}\``;
+					}
 
 					if (displayOptions.parameters && doc.parameter) {
 						for (let parameter of doc.parameter) {
-								if (displayOptions.asDocString) {
-									description += '/// ';
-								}
-								description += `@param ${parameter.type ? `{${parameter.type}}` : ''} \`${parameter.name}\`${parameter.description ? ` - ${parameter.description}` : ''}\n`;
+							description += `\n\n@param ${parameter.type ? `{${parameter.type}}` : ''} \`${parameter.name}\`${parameter.description ? ` - ${parameter.description}` : ''}`;
 						}
 					}
 
 					if (displayOptions.return && doc.return) {
-							if (displayOptions.asDocString) {
-								description += '/// ';
-							}
-							description += `@return {${doc.return.type}}\n`;
+						description += `\n\n@return {\`${doc.return.type}\`}`;
 					}
 
 					return description;

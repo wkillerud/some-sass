8e18ad9bd4cb4f5751b403d42e3e426f775471ed
William Killerud <william@killerud.com>
2022-05-18 20:31:29 +0200

    refactor: scanner ignores unused partials by default

--- src/unsafe/services/scanner.ts
+++ src/unsafe/services/scanner.ts
@@ -12,39 +12,52 @@ export default class ScannerService {
 	constructor(private readonly _storage: StorageService, private readonly _settings: ISettings) {}
 
 	public async scan(files: string[], recursive = true): Promise<void> {
-		const iterator = new Set(files);
+		let uniqueFiles = [...new Set(files)];
+		if (this._settings.scanImportedFiles) {
+			// If we scan imported files (which we do by default), don't include partials in the initial scan.
+			// This way we can be reasonably sure that we scan whatever index files there are _before_ we scan
+			// partials which may or may not have been forwarded with a prefix.
+			uniqueFiles = uniqueFiles.filter((file) => !file.includes("/_") && !file.includes("\\_"));
+		}
+		await Promise.all(
+			uniqueFiles.map((path) => {
+				return this.parse(path, recursive);
+			})
+		);
+	}
 
-		for (let filepath of iterator) {
-			// Cast to the system file path style
-			filepath = path.normalize(filepath);
+	protected async parse(filepath: string, recursive: boolean): Promise<void> {
+		// Cast to the system file path style
+		filepath = path.normalize(filepath);
+		const uri = URI.file(filepath).toString();
 
-			const uri = URI.file(filepath).toString();
+		const isExistFile = await this._fileExists(filepath);
+		if (!isExistFile) {
+			this._storage.delete(uri);
+			return;
+		}
 
-			const isExistFile = await this._fileExists(filepath);
+		const alreadyParsed = this._storage.get(uri);
+		if (alreadyParsed) {
+			return;
+		}
 
-			if (!isExistFile) {
-				this._storage.delete(uri);
+		const content = await this._readFile(filepath);
+		const document = TextDocument.create(uri, 'scss', 1, content);
+		const { symbols } = await parseDocument(document, null);
 
+		this._storage.set(uri, { ...symbols, filepath });
+
+		if (!recursive || !this._settings.scanImportedFiles) {
+			return;
+		}
+
+		for (const symbol of symbols.imports) {
+			if (symbol.dynamic || symbol.css) {
 				continue;
 			}
 
-			const content = await this._readFile(filepath);
-			const document = TextDocument.create(uri, 'scss', 1, content);
-			const { symbols } = await parseDocument(document, null);
-
-			this._storage.set(uri, { ...symbols, filepath });
-
-			if (!recursive || !this._settings.scanImportedFiles) {
-				continue;
-			}
-
-			for (const symbol of symbols.imports) {
-				if (symbol.dynamic || symbol.css) {
-					continue;
-				}
-
-				iterator.add(symbol.filepath);
-			}
+			await this.parse(symbol.filepath, recursive);
 		}
 	}
 

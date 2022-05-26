7ae4bb1edb02c54913fe3bd48420aa66c89d6371
William Killerud <william@killerud.com>
2022-05-26 12:02:50 +0200

    feat: diagnostics understand prefixes

--- src/unsafe/providers/diagnostics.ts
+++ src/unsafe/providers/diagnostics.ts
@@ -2,7 +2,7 @@ import { Diagnostic, DiagnosticSeverity, DiagnosticTag, VersionedTextDocumentIde
 import { EXTENSION_NAME } from '../../constants';
 import type StorageService from '../services/storage';
 import { INode, NodeType } from '../types/nodes';
-import type { ScssFunction, ScssMixin, ScssVariable } from '../types/symbols';
+import type { IScssDocument, ScssForward, ScssImport, ScssSymbol } from '../types/symbols';
 
 export async function doDiagnostics(
   document: VersionedTextDocumentIdentifier,
@@ -10,61 +10,19 @@ export async function doDiagnostics(
 ): Promise<Diagnostic[]> {
   const diagnostics: Diagnostic[] = [];
 
-  const currentScssDocument = storage.get(document.uri);
-  if (!currentScssDocument) {
+  const openDocument = storage.get(document.uri);
+  if (!openDocument) {
     console.error("Tried to do diagnostics on a document that has not been scanned. This should never happen.");
     return diagnostics;
   }
 
-  const references: INode[] = getVariableFunctionMixinReferences(currentScssDocument.ast);
+  const references: INode[] = getVariableFunctionMixinReferences(openDocument.ast);
   if (references.length === 0) {
     return diagnostics;
   }
 
-
   for (const node of references) {
-    const identifier = {
-      type: node.type,
-      name: node.getName(),
-    };
-
-    const scssDocuments = storage.values();
-    for (const scssDocument of scssDocuments) {
-      if (identifier.type === NodeType.VariableName && scssDocument.variables.has(identifier.name)) {
-        const variable = scssDocument.variables.get(identifier.name) as ScssVariable;
-        if (typeof variable.sassdoc?.deprecated !== 'undefined') {
-          diagnostics.push({
-            message: variable.sassdoc.deprecated || `${identifier.name} is deprecated`,
-            range: currentScssDocument.getNodeRange(node),
-            source: EXTENSION_NAME,
-            tags: [DiagnosticTag.Deprecated],
-            severity: DiagnosticSeverity.Hint,
-          })
-        }
-      } else if (identifier.type === NodeType.MixinReference && scssDocument.mixins.has(identifier.name)) {
-        const mixin = scssDocument.mixins.get(identifier.name) as ScssMixin;
-        if (typeof mixin.sassdoc?.deprecated !== 'undefined') {
-          diagnostics.push({
-            message: mixin.sassdoc.deprecated || `${identifier.name} is deprecated`,
-            range: currentScssDocument.getNodeRange(node),
-            source: EXTENSION_NAME,
-            tags: [DiagnosticTag.Deprecated],
-            severity: DiagnosticSeverity.Hint,
-          })
-        }
-      } else if (identifier.type === NodeType.Function && scssDocument.functions.has(identifier.name)) {
-        const func = scssDocument.functions.get(identifier.name) as ScssFunction;
-        if (typeof func.sassdoc?.deprecated !== 'undefined') {
-          diagnostics.push({
-            message: func.sassdoc.deprecated || `${identifier.name} is deprecated`,
-            range: currentScssDocument.getNodeRange(node),
-            source: EXTENSION_NAME,
-            tags: [DiagnosticTag.Deprecated],
-            severity: DiagnosticSeverity.Hint,
-          })
-        }
-      }
-    }
+    doSymbolHunting(openDocument, storage, diagnostics, node);
   }
 
   return diagnostics;
@@ -91,4 +49,67 @@ function getVariableFunctionMixinReferences(fromNode: INode): INode[] {
       }
       return getVariableFunctionMixinReferences(child);
     });
+}
+
+function doSymbolHunting(openDocument: IScssDocument, storage: StorageService, result: Diagnostic[], node: INode): Diagnostic[] {
+  traverseTree(openDocument, openDocument, storage, result, node);
+	return result;
+}
+
+function traverseTree(openDocument: IScssDocument, childDocument: IScssDocument, storage: StorageService, result: Diagnostic[], node: INode, accumulatedPrefix = ""): Diagnostic[] {
+	const scssDocument = storage.get(childDocument.uri);
+	if (!scssDocument) {
+		return result;
+	}
+
+  const map = node.type === NodeType.Function
+    ? scssDocument.functions
+    : node.type === NodeType.MixinReference
+      ? scssDocument.mixins
+      : scssDocument.variables;
+
+
+  // Entry has prefix, so we need to strip that prefix away from entry
+  // before looking for a symbol as it has been named in the original document.
+  const symbolName = node.getName().replace(accumulatedPrefix, "");
+  if (map.has(symbolName)) {
+    const diagnostic = createDiagnostic(openDocument, node, map.get(symbolName)!);
+    if (diagnostic) {
+      result.push(diagnostic);
+    }
+  }
+
+	// Check to see if we have to go deeper
+	for (const child of scssDocument.getLinks()) {
+		if (!child.link.target || (child as ScssImport).dynamic || (child as ScssImport).css) {
+			continue;
+		}
+
+		const childDocument = storage.get(child.link.target);
+		if (!childDocument) {
+			continue;
+		}
+
+		let prefix = accumulatedPrefix;
+		if ((child as ScssForward).prefix) {
+			prefix += (child as ScssForward).prefix;
+		}
+
+		traverseTree(openDocument, childDocument, storage, result, node, prefix);
+	}
+
+	return result;
+}
+
+function createDiagnostic(openDocument: IScssDocument, node: INode, symbol: ScssSymbol): Diagnostic | null {
+  if (typeof symbol.sassdoc?.deprecated !== 'undefined') {
+    return {
+      message: symbol.sassdoc.deprecated || `${symbol.name} is deprecated`,
+      range: openDocument.getNodeRange(node),
+      source: EXTENSION_NAME,
+      tags: [DiagnosticTag.Deprecated],
+      severity: DiagnosticSeverity.Hint,
+    }
+  }
+  return null;
 }

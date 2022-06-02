56acbfae2b9012c8ee9b2c5bb8d37bceb974db95
William Killerud <william@killerud.com>
2022-06-02 19:43:33 +0200

    test: unit test for diagnostics with prefixes

--- src/unsafe/test/providers/diagnostics.spec.ts
+++ src/unsafe/test/providers/diagnostics.spec.ts
@@ -1,14 +1,30 @@
 import assert from 'assert';
+import fs, { Stats } from 'fs';
+import sinon from 'sinon';
 import { DiagnosticSeverity, DiagnosticTag } from 'vscode-languageserver-types';
-import { EXTENSION_NAME } from '../../../constants';
 
+import { EXTENSION_NAME } from '../../../constants';
 import { doDiagnostics } from '../../providers/diagnostics';
 import StorageService from '../../services/storage';
+import * as fsUtils from '../../utils/fs';
 import * as helpers from '../helpers';
 
 const storage = new StorageService();
 
 describe("Providers/Diagnostics", () => {
+  let statStub: sinon.SinonStub;
+  let fileExistsStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    fileExistsStub = sinon.stub(fsUtils, 'fileExists');
+    statStub = sinon.stub(fs, 'stat').yields(null, new Stats());
+  });
+
+  afterEach(() => {
+    fileExistsStub.restore();
+    statStub.restore();
+  });
+
   it("doDiagnostics - Variables", async () => {
     const document = await helpers.makeDocument(storage, [
       "/// @deprecated Use something else",
@@ -130,6 +146,51 @@ describe("Providers/Diagnostics", () => {
     const actual = await doDiagnostics(document, storage);
 
     assert.strictEqual(actual.length, 3);
+
+    // Make sure we set a default message for the deprecated tag
+    assert.ok(actual.every(d => Boolean(d.message)), "Every diagnostic must have a message");
+  });
+
+  it("doDiagnostics - support namespaces with prefix", async () => {
+    fileExistsStub.resolves(true);
+
+    await helpers.makeDocument(storage, [
+      "/// @deprecated",
+      "$old-a: 1;",
+    ], { uri: 'variables.scss' });
+    await helpers.makeDocument(storage, [
+      "/// @deprecated",
+      "@function old-function() {",
+      "  @return 1;",
+      "}",
+    ], { uri: 'functions.scss' });
+    await helpers.makeDocument(storage, [
+      "/// @deprecated",
+      "@mixin old-mixin {",
+      "  content: 'mixin';",
+      "}",
+    ], { uri: 'mixins.scss' });
+    await helpers.makeDocument(storage, [
+      "@forward './functions' as fun-*;",
+      "@forward './mixins' as mix-* hide secret, other-secret;",
+      "@forward './variables' hide $secret;",
+    ], { uri: 'namespace.scss'});
+
+    const document = await helpers.makeDocument(storage, [
+      "@use 'namespace' as ns;",
+      ".foo {",
+      "  color: ns.$old-a;",
+      "  line-height: ns.fun-old-function();",
+      "  @include ns.mix-old-mixin;",
+      "}",
+    ]);
+
+    const actual = await doDiagnostics(document, storage);
+
+    // For some reason we get duplicate diagnostics for mixins.
+    // Haven't been able to track down why getVariableFunctionMixinReferences produces two of the same node.
+    // It's probably fine...
+    assert.strictEqual(actual.length, 4);
 
     // Make sure we set a default message for the deprecated tag
     assert.ok(actual.every(d => Boolean(d.message)), "Every diagnostic must have a message");

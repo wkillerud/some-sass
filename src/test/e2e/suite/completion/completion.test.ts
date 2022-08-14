d4eb8eb1ae97bb531b45de0d54417c22141da18a
William Killerud <william@killerud.com>
2022-08-14 15:33:06 +0200

    fix: suggestions in return context
    Adjust test case now that we know the actual issue

--- src/test/e2e/suite/completion/completion.test.ts
+++ src/test/e2e/suite/completion/completion.test.ts
@@ -228,24 +228,22 @@ describe("SCSS Completion Test", function () {
 		await testCompletion(docUri, position(26, 20), expectedCompletions);
 	});
 
-	it("Offers namespace completion as part of math expression", async () => {
-		for (const line of [30, 34, 38]) {
-			const expectedCompletions = [
-				{
-					label: "$var-var-variable",
-					detail: "Variable declared in _variables.scss",
-					insertText: '".$var-var-variable"',
-					filterText: '"ns.$var-var-variable"',
-				},
-				{
-					label: "fun-fun-function",
-					detail: "Function declared in _functions.scss",
-					insertText: '{"_tabstop":1,"value":".fun-fun-function()"}',
-				},
-			];
+	it("Offers namespace completion as part of return statement", async () => {
+		const expectedCompletions = [
+			{
+				label: "$var-var-variable",
+				detail: "Variable declared in _variables.scss",
+				insertText: '".$var-var-variable"',
+				filterText: '"ns.$var-var-variable"',
+			},
+			{
+				label: "fun-fun-function",
+				detail: "Function declared in _functions.scss",
+				insertText: '{"_tabstop":1,"value":".fun-fun-function()"}',
+			},
+		];
 
-			await testCompletion(docUri, position(line, 23), expectedCompletions);
-		}
+		await testCompletion(docUri, position(30, 23), expectedCompletions);
 	});
 });
 

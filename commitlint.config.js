35617e3142aed74d764f972b7e0b719ec37ee4fa
William Killerud <william@killerud.com>
2024-03-02 16:23:45 +0100

    chore: configure commitlint to let release commits pass

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+/** @type {import("@commitlint/config-conventional")} */
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"header-max-length": [0],
+		"body-max-line-length": [0],
+		"footer-max-line-length": [0],
+	},
+};

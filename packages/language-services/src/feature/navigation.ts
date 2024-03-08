7aecca8be05d40f6a25d81cbfa295ba23330e42c
William Killerud <william@killerud.com>
2024-03-08 22:26:46 +0100

    feat: include the namespace, either as or derived

--- packages/language-services/src/feature/navigation.ts
+++ packages/language-services/src/feature/navigation.ts
@@ -153,11 +153,17 @@ export class SassNavigation {
 								}
 							}
 
+							const namespace =
+								cursor.node.type.name === SyntaxNodeType.UseStatement
+									? as || getNamespaceFromLink(target)
+									: undefined;
+
 							unresolved.push({
 								link: {
 									...showHide,
 									type: cursor.node.type.name,
 									as,
+									namespace,
 									target,
 									range: Range.create(
 										document.positionAt(from),
@@ -579,4 +585,33 @@ export class SassNavigation {
 			subpathObject["default"]
 		);
 	}
+}
+
+function getNamespaceFromLink(target: string): string {
+	if (target.startsWith("sass")) {
+		return target.split(":")[1];
+	}
+
+	const bareTarget = target.replace("pkg:", "").replace("./", "");
+	let from = 0;
+	let to = bareTarget.length;
+	if (bareTarget.includes("/")) {
+		from = bareTarget.lastIndexOf("/") + 1;
+	}
+	if (bareTarget.includes(".")) {
+		to = bareTarget.lastIndexOf(".");
+	}
+	let namespace = bareTarget.substring(from, to);
+	namespace = namespace.startsWith("_") ? namespace.slice(1) : namespace;
+	if (namespace === "index") {
+		// The link points to an index file. Use the folder name above as a namespace.
+		const linkOmitIndex = bareTarget.slice(
+			0,
+			Math.max(0, bareTarget.lastIndexOf("/")),
+		);
+		const newLastSlash = linkOmitIndex.lastIndexOf("/");
+		namespace = linkOmitIndex.slice(Math.max(0, newLastSlash + 1));
+	}
+
+	return namespace;
 }

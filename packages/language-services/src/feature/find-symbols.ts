e9deee2a04de6b6e88753dc2f09d60e836eaad5f
William Killerud <william@killerud.com>
2024-03-09 11:46:39 +0100

    chore: todo

--- packages/language-services/src/feature/find-symbols.ts
+++ packages/language-services/src/feature/find-symbols.ts
@@ -7,43 +7,7 @@ import {
 	TextDocument,
 	TreeCursor,
 } from "@somesass/language-server-types";
-
-// export interface DocumentSymbol {
-// 	/**
-// 	 * The name of this symbol. Will be displayed in the user interface and therefore must not be
-// 	 * an empty string or a string only consisting of white spaces.
-// 	 */
-// 	name: string;
-// 	/**
-// 	 * More detail for this symbol, e.g the signature of a function.
-// 	 */
-// 	detail?: string;
-// 	/**
-// 	 * The kind of this symbol.
-// 	 */
-// 	kind: SymbolKind;
-// 	/**
-// 	 * Tags for this document symbol.
-// 	 *
-// 	 * @since 3.16.0
-// 	 */
-// 	tags?: SymbolTag[];
-// 	/**
-// 	 * The range enclosing this symbol not including leading/trailing whitespace but everything else
-// 	 * like comments. This information is typically used to determine if the clients cursor is
-// 	 * inside the symbol to reveal in the symbol in the UI.
-// 	 */
-// 	range: Range;
-// 	/**
-// 	 * The range that should be selected and revealed when this symbol is being picked, e.g the name of a function.
-// 	 * Must be contained by the `range`.
-// 	 */
-// 	selectionRange: Range;
-// 	/**
-// 	 * Children of this symbol, e.g. properties of a class.
-// 	 */
-// 	children?: DocumentSymbol[];
-// }
+import { SymbolTag } from "vscode";
 
 export class SassSymbolFinder {
 	findDocumentSymbols(
@@ -71,18 +35,64 @@ function findTreeSymbols(
 				tree.node.firstChild.cursor(),
 			);
 		}
+
 		const from = tree.node.from;
 		const to = tree.node.to;
+
 		const type = tree.node.type.name as SyntaxNodeType;
+		const kind = typeToKind(type);
+
+		// TODO:
+		// These are the ones in use by the language server _today_
+		// - [/] Sass variable. SymbolKind.Variable.
+		// - [/] Sass mixin. SymbolKind.Method.
+		// - [/] Sass function. SymbolKind.Function.
+		// - [/] CSS Selector. SymbolKind.Class.
+		// These are the ones in use by vscode-css-languageservice for feature parity
+		// - [/] Keyframe at rule. SymbolKind.Class.
+		// - [/] Fontface at rule. SymbolKind.Class.
+		// - [/] Media at rule. SymbolKind.Module.
+
+		/**
+		 * The name of this symbol. Will be displayed in the user interface and therefore must not be
+		 * an empty string or a string only consisting of white spaces.
+		 */
+		const name: string = "<undefined>";
+
+		/**
+		 * More detail for this symbol, e.g the signature of a function.
+		 */
+		const detail: string | undefined = undefined;
+
+		/**
+		 * Tags such as deprecated.
+		 */
+		const tags: SymbolTag[] = [];
+
+		/**
+		 * The range enclosing this symbol not including leading/trailing whitespace but everything else
+		 * like comments. This information is typically used to determine if the clients cursor is
+		 * inside the symbol to reveal in the symbol in the UI.
+		 */
+		const range = Range.create(
+			document.positionAt(from),
+			document.positionAt(to),
+		);
+
+		/**
+		 * The range that should be selected and revealed when this symbol is being picked, e.g the name of a function.
+		 * Must be contained by the `range`.
+		 */
+		const selectionRange = range;
+
 		symbols.push({
-			name: source.substring(from, to),
+			name,
+			detail,
+			kind,
 			type,
-			kind: typeToKind(type),
-			range: Range.create(document.positionAt(from), document.positionAt(to)),
-			selectionRange: Range.create(
-				document.positionAt(from),
-				document.positionAt(to),
-			),
+			tags,
+			range,
+			selectionRange,
 			children,
 		});
 	}
@@ -90,39 +100,28 @@ function findTreeSymbols(
 }
 
 /**
- * // TODO: flesh this one out once you know which SyntaxNodeType are relevant
- * Returns one of these SymbolKind, defaulting to SymbolKind.String. Match the expected SymbolKind from vscode-css-languageservice
- * - SymbolKind.Module,
- * - SymbolKind.Namespace,
- * - SymbolKind.Class,
- * - SymbolKind.Method,
- * - SymbolKind.Property,
- * - SymbolKind.Field,
- * - SymbolKind.Constructor,
- * - SymbolKind.Enum,
- * - SymbolKind.Function,
- * - SymbolKind.Variable,
- * - SymbolKind.Constant,
- * - SymbolKind.String,
- * - SymbolKind.Number,
- * - SymbolKind.Boolean,
- * - SymbolKind.Array,
- * - SymbolKind.Object,
- * - SymbolKind.Key,
- * - SymbolKind.Null,
- * - SymbolKind.Operator
- *
- * @param type
- * @returns
+ * Translate  @lezer/sass SyntaxNodeType to vscode-languageserver-types SymbolKind
  */
 function typeToKind(type: SyntaxNodeType): SymbolKind {
 	switch (type) {
+		case SyntaxNodeType.mixin:
+			return SymbolKind.Method;
+
+		case SyntaxNodeType.SassVariableName:
+			return SymbolKind.Variable;
+
 		case SyntaxNodeType.MediaStatement:
 			return SymbolKind.Module;
+
 		case SyntaxNodeType.CallExpression:
+		case SyntaxNodeType.CallLiteral:
 			return SymbolKind.Method;
-		case SyntaxNodeType.ClassName:
+
+		case SyntaxNodeType.selector:
+		case SyntaxNodeType.AtRule:
+		case SyntaxNodeType.KeyframesStatement:
 			return SymbolKind.Class;
+
 		case SyntaxNodeType.StringLiteral:
 		default:
 			return SymbolKind.String;

27a15a4819049e0b0aa7a36126c782476e39389d
William Killerud <william@killerud.com>
2024-05-24 22:33:34 +0200

    test: indented selector printing

--- packages/vscode-css-languageservice/src/test/sass/selectorPrinting.test.ts
+++ packages/vscode-css-languageservice/src/test/sass/selectorPrinting.test.ts
@@ -59,27 +59,121 @@ suite("Sass - Selector Printing", () => {
 	});
 
 	test("nested selector", function () {
-		let p = new SassParser();
-		assertSelector(p, "o1 { e1 { } }", "e1", "{o1{…{e1}}}");
-		assertSelector(p, "o1 { e1.div { } }", "e1", "{o1{…{e1[class=div]}}}");
-		assertSelector(p, "o1 o2 { e1 { } }", "e1", "{o1{…{o2{…{e1}}}}}");
-		assertSelector(p, "o1, o2 { e1 { } }", "e1", "{o1{…{e1}}}");
-		assertSelector(p, "o1 { @if $a { e1 { } } }", "e1", "{o1{…{e1}}}");
-		assertSelector(p, "o1 { @mixin a { e1 { } } }", "e1", "{e1}");
-		assertSelector(p, "o1 { @mixin a { e1 { } } }", "e1", "{e1}");
+		let p = new SassParser({ syntax: "indented" });
+		assertSelector(
+			p,
+			`o1
+	e1
+		//`,
+			"e1",
+			"{o1{…{e1}}}",
+		);
+		assertSelector(
+			p,
+			`o1
+	e1.div
+		//`,
+			"e1",
+			"{o1{…{e1[class=div]}}}",
+		);
+		assertSelector(
+			p,
+			`o1 o2
+	e1
+		//`,
+			"e1",
+			"{o1{…{o2{…{e1}}}}}",
+		);
+		assertSelector(
+			p,
+			`o1, o2
+	e1
+		//`,
+			"e1",
+			"{o1{…{e1}}}",
+		);
+		assertSelector(
+			p,
+			`o1
+	@if $a
+		e1
+			//`,
+			"e1",
+			"{o1{…{e1}}}",
+		);
+		assertSelector(
+			p,
+			`o1
+	@mixin a
+		e1
+			//`,
+			"e1",
+			"{e1}",
+		);
+		assertSelector(
+			p,
+			`o1
+	@mixin a
+		e1
+			//`,
+			"e1",
+			"{e1}",
+		);
 	});
 
 	test("referencing selector", function () {
-		let p = new SassParser();
-		assertSelector(p, "o1 { &:hover { }}", "&", "{o1[:hover=]}");
-		assertSelector(p, "o1 { &:hover & { }}", "&", "{o1[:hover=]{…{o1}}}");
-		assertSelector(p, "o1 { &__bar {}}", "&", "{o1__bar}");
-		assertSelector(p, ".c1 { &__bar {}}", "&", "{[class=c1__bar]}");
-		assertSelector(p, "o.c1 { &__bar {}}", "&", "{o[class=c1__bar]}");
+		let p = new SassParser({ syntax: "indented" });
+		assertSelector(
+			p,
+			`o1
+	&:hover
+		//`,
+			"&",
+			"{o1[:hover=]}",
+		);
+		assertSelector(
+			p,
+			`o1
+	&:hover &
+		//`,
+			"&",
+			"{o1[:hover=]{…{o1}}}",
+		);
+		assertSelector(
+			p,
+			`o1
+	&__bar
+		//`,
+			"&",
+			"{o1__bar}",
+		);
+		assertSelector(
+			p,
+			`.c1
+	&__bar
+		//`,
+			"&",
+			"{[class=c1__bar]}",
+		);
+		assertSelector(
+			p,
+			`o.c1
+	&__bar
+		//`,
+			"&",
+			"{o[class=c1__bar]}",
+		);
 	});
 
 	test("placeholders", function () {
-		let p = new SassParser();
-		assertSelector(p, "%o1 { e1 { } }", "e1", "{%o1{…{e1}}}");
+		let p = new SassParser({ syntax: "indented" });
+		assertSelector(
+			p,
+			`%o1
+	e1
+		//`,
+			"e1",
+			"{%o1{…{e1}}}",
+		);
 	});
 });
